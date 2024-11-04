6fda2c64519509fed22f26192d05ac20c2629718
dlo0lb <168185779+dlo0lb@users.noreply.github.com>
2024-11-04 03:18:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // Инициализируем объект WebApp
-const tg = window.Telegram.WebApp;
+const tg = window.Telegram ? window.Telegram.WebApp : null;
 
-// Добавляем обработчик клика на кнопку
-document.getElementById('start-button').addEventListener('click', () => {
-  tg.MainButton.setText("Let's go!");
-  tg.MainButton.show();
-  tg.MainButton.onClick(() => {
-    tg.sendData("Start learning"); // Отправляем данные боту
-    tg.MainButton.hide(); // Скрываем кнопку после отправки данных
+// Код для главной страницы
+if (document.getElementById('start-button')) {
+  document.getElementById('start-button').addEventListener('click', () => {
+    if (tg) {
+      tg.MainButton.setText("Let's go!");
+      tg.MainButton.show();
+      tg.MainButton.onClick(() => {
+        tg.sendData("Start learning"); // Отправляем данные боту
+        tg.MainButton.hide(); // Скрываем кнопку после отправки данных
+      });
+    }
   });
-});
+}
+
+// Код для страницы уроков
+function goToLesson(lesson) {
+  // Здесь вы можете реализовать логику перехода к конкретным урокам
+  alert(`Переход к уроку: ${lesson}`);
+}
